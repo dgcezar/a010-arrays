eed0f76ebd80668bb6e01ea9d77762f406cc7d17
dgcezar <diego.cezar13@gmail.com>
2022-09-01 12:24:27 -0300

    att1 - exercício 2 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,12 @@
-const array1 = [12,34,56];
+const array1 = [12, 34, 56];
 const array2 = ["olá", "labenu", "opa"];
 const array3 = [123, "14", false];
 
 console.log(array1, array2, array3);
+
+console.log(array1[0], array2[1], array3[2]);
+
+array1.push(32);
+
+console.log(array1.includes(32));
+console.log(array3.includes(442));
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
 
-console.log 
+const array1 = [12,34,56];
+const array2 = ["olá", "labenu", "opa"];
+const array3 = [123, "14", false];
+
+console.log(array1.length, array2.length, array3.length);
+
+console.log(array1[0], array2[1], array3[3]);
